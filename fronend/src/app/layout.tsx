2279269b03df045e56488c4f07fdecec8a372cf3
joshua-inf <jsibanda407@gmail.com>
2025-02-18 12:39:44 +0200

    added frontent

---
+++ fronend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "The Might Database",
+  description: "for CBU by CBU",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-5`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
