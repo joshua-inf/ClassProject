2ab2125262aa6ec76188405a3412f5a01de3249e
2004-zm <zangamukanu69@gmai.com>
2025-03-28 09:37:02 +0100

    ok

--- Djbackend/clinicm/api/views.py
+++ Djbackend/clinicm/api/views.py
@@ -56,8 +56,14 @@ def login_clinician(request):
     if serializer.is_valid():
         user = serializer.validated_data
         token, created = Token.objects.get_or_create(user=user)
-
-        return Response({"token": token.key})
+        user_serializer = CustomUserSerializer(user)
+        return Response({'user': {
+                    'email': user_serializer.email,
+                    'first_name': user_serializer.first_name,
+                    'last_name': user_serializer.last_name,
+                    'specialty': user_serializer.specialty,
+                    'phone_number': user_serializer.phone_number,
+                },"token": token.key})
     
     return Response({"error": "Invalid credentials"}, status=400)
 
